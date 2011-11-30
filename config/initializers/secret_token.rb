e96734206e4859371176188a9861b14979836892
jaipratik <jp_ca@live.com>
2011-11-29 21:34:38 -0800

    commit after controller film

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5video::Application.config.secret_token = '3b095093b3d8cd6cca69e8366e4b5c565ec5e724c58544a96e11317193a2a8677c6d6ff1fe0390bc67f8a0bec9066a151ce2ddc056873c5af56f0bf0ea6eea4c'
